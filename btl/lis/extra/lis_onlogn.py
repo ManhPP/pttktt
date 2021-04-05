e040ea4b72a7247c4a4a7404eb78adacd00a8bdd
manhpp <phumanh1998@gmail.com>
2021-04-05 23:58:34 +0700

    update

--- btl/lis/extra/lis_onlogn.py
+++ btl/lis/extra/lis_onlogn.py
@@ -11,41 +11,25 @@ def get_ceil_index(arr, T, l, r, key):
 
 
 def lis(arr, n):
-    # Add boundary case,
-    # when array n is zero
-    # Depend on smart pointers
-
-    # Initialized with 0
     tail_indices = [0 for i in range(n + 1)]
 
-    # Initialized with -1
     prev_indices = [-1 for i in range(n + 1)]
 
-    # it will always point
-    # to empty location
     len = 1
     for i in range(1, n):
 
         if arr[i] < arr[tail_indices[0]]:
 
-            # new smallest value
             tail_indices[0] = i
 
         elif arr[i] > arr[tail_indices[len - 1]]:
 
-            # arr[i] wants to extend
-            # largest subsequence
             prev_indices[i] = tail_indices[len - 1]
             tail_indices[len] = i
             len += 1
 
         else:
 
-            # arr[i] wants to be a
-            # potential condidate of
-            # future subsequence
-            # It will replace ceil
-            # value in tail_indices
             pos = get_ceil_index(arr, tail_indices, -1,
                                  len - 1, arr[i])
 
